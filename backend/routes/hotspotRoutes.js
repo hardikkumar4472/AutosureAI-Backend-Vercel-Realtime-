4fa11739dc76e791032b2a0adb12f433f9ad87cd
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-12-04 13:55:43 +0530

    Delete backend directory

--- backend/routes/hotspotRoutes.js
+++
@@ -1,8 +0,0 @@
-
-import express from "express";
-import { getPublicHotspots } from "../controllers/hotspotController.js";
-const router = express.Router();
-
-router.get("/hotspots", getPublicHotspots);
-
-export default router;
--- backend/services/socketBridge.js
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-const flaskSocket = io(process.env.FLASK_SOCKET_URL, { transports: ["websocket"] });
-flaskSocket.on("connect", () => console.log("Connected to Flask Microservice"));
-flaskSocket.on("disconnect", () => console.log("Disconnected from Flask"));
-export default flaskSocket;
