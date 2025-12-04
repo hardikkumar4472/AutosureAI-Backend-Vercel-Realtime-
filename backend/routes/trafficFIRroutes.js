0cf1f8c874918ce46aa30285c73cf2a352b65b3a
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-12-04 21:18:14 +0530

    Update trafficFIRroutes.js

--- backend/routes/trafficFIRroutes.js
+++ backend/routes/trafficFIRroutes.js
@@ -3,7 +3,7 @@ import multer from "multer";
 import { trafficAuth } from "../middleware/trafficAuth.js";
 import { updateFIR } from "../controllers/trafficController.js";
 
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 
 const router = express.Router();
 
